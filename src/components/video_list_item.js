d568af6315e4ad2c405a95791abc2fe8ea9d714d
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-29 13:39:11 -0800

    Lecture 26: show thumbnails and titles for each video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
+const VideoListItem = ({ video }) => {
+  const imageURL = video.snippet.thumbnails.default.url
+
   return (
-    <li>Video</li>
+    <li className='list-group-item'>
+      <div className='video-list media'>
+        <div className='media-left'>
+          <div className='media-object'>
+            <img src={ imageURL }/>
+          </div>
+        </div>
+        <div className='media-body'>
+          <div className='media-heading'>
+            { video.snippet.title }
+          </div>
+        </div>
+      </div>
+    </li>
   )
 }
 
