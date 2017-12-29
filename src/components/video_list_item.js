334fc5093c70b0389d3e37d9541fbafd11656d55
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-29 14:09:09 -0800

    Lesson 29: Select a video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   const imageURL = video.snippet.thumbnails.default.url
 
   return (
-    <li className='list-group-item'>
+    <li 
+      onClick={() => onVideoSelect(video) }
+      className='list-group-item'>
       <div className='video-list media'>
         <div className='media-left'>
           <div className='media-object'>
