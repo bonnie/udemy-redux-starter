cd8e87906b324960faf6e1a4a9454bdfa4b76e5f
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-31 11:24:10 -0800

    Add note about react-google-maps

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+// note: there's a package: react-google-maps that makes this easier... ostensibly. I couldn't get it working
+// even with the exact code from the tutorial. >_<
 
 class GoogleMap extends Component {
   componentDidMount() {
