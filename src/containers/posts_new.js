51b72062fa32fb21d28154a161498ab0c91d0376
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-31 14:00:24 -0800

    Lesson 130: Create PostsNew component skeleton and implement Switch router component

---
+++ src/containers/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    )
+  }
+}
+
+export default PostsNew
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 // route is a react component that provides configuration for url -> component
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
 
-import PostsIndex from './components/posts_index'
-import PostsShow from './components/posts_show'
-import PostsNew from './components/posts_new'
+import PostsIndex from './containers/posts_index'
+import PostsShow from './containers/posts_show'
+import PostsNew from './containers/posts_new'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -17,9 +17,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route exact path="/" component={PostsIndex} />
-        <Route path="/posts/:id" component={PostsShow} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route exact path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
