c85d51989cc35765a8a853a1f43d690134fa4e1a
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-31 12:15:10 -0800

    Lesson 121: Create simple routing for /hello and /goodbye

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+// route is a react component that provides configuration for url -> component
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+// for testing
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
