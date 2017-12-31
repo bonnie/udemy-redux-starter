5f4df59dd472851591de74c64c89d118d226e7d3
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-12-31 13:32:04 -0800

    Lesson 128: Create action / reducer to fetch posts, and display in posts_index component

---
+++ src/components/posts_index.js
@@ -0,0 +1,46 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
+
+class PostsIndex extends Component {
+  // react lifecycle function
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
+  renderPosts() {
+    if (!this.props) {
+      return <div>Loading...</div>
+    }
+    if (!this.props.posts) {
+      return <div>No posts found</div>
+    }
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts }
+}
+
+// instead of using mapDispatchToProps, you can just use an object
+// for that argument as a shortcut
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 // route is a react component that provides configuration for url -> component
 import { BrowserRouter, Route } from 'react-router-dom'
+import promise from 'redux-promise'
 
-import PostsIndex from './containers/posts_index'
-import PostsShow from './containers/posts_show'
+import PostsIndex from './components/posts_index'
+import PostsShow from './components/posts_show'
 import PostsNew from './components/posts_new'
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
